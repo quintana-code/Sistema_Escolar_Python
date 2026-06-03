4f72f7fbc654be492c5a6bd6cf4c712ba3af3903
Linn <quintana.hernandez.geraline@gmail.com>
2026-06-03 15:55:50 -0600

    Actualizacion de conexion con validacion

--- conexion/conexion.py
+++ conexion/conexion.py
@@ -4,9 +4,14 @@ class Conexion:
     @staticmethod
     #Definiendo que esta clase no tiene algo que definir con el atributo 'self'
     def conectar():
-        conexion= mysql.connector.connect(user='root', password='',
+        try:
+            conexion= mysql.connector.connect(user='root', password='',
                                   host='localhost',
                                   database='bdasistencia', 
                                   port= '3306')
-        return conexion
+            print("Conexión exitosa")
+            return conexion
+        
+        except mysql.connector.Error as e:
+            print("Error en la conexión: ",e)
     
